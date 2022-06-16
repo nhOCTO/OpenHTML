be3ce1d4a89251960f6477d8fe37449c6f834f51
Code Ninjas 12 <Code Ninjas 12@LAPTOP-V9U5LGSV>
2022-06-16 18:28:08 -0400

    Add project files.

---
+++ OpenHTML/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenHTML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            label1.Text = webBrowser1.DocumentTitle;
+        }
+
+        private void googleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("www.google.com");
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void reloadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void gmailToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("www.gmail.com");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
